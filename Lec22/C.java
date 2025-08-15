fffa129cbccc38548378fa27acfc426c9147a29d
monukumar98 <monu012as@gmail.com>
2025-08-15 09:31:35 +0530

    P&C

---
+++ Lec22/C.java
@@ -0,0 +1,12 @@
+package Lec22;
+
+public class C {
+	int d = 2;
+	int d2 = 20;
+	public void fun() {
+		System.out.println("FUN IN C");
+	}
+	public void fun2() {
+		System.out.println("FUN2 IN P");
+	}
+}
---
+++ Lec22/P.java
@@ -0,0 +1,14 @@
+package Lec22;
+
+public class P {
+	int d = 1;
+	int d1 = 10;
+	
+	public void fun() {
+		System.out.println("FUN IN P");
+	}
+	public void fun1() {
+		System.out.println("FUN1 IN P");
+	}
+
+}
